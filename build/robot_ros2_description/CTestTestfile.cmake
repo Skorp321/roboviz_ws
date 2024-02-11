edf1ea81d2b6b7c714c53091a747827b428339e6
Skorp321 <skorp321@yandex.ru>
2024-02-11 16:29:03 +0300

    create visualisation of simple robot in rviz

---
+++ build/robot_ros2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/skorp321/roboviz_ws/src/robot_ros2_description
+# Build directory: /home/skorp321/roboviz_ws/build/robot_ros2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
